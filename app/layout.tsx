5c9603939b034bbbdebdfe4499946069f21d6b08
Abhishek Ray <abhishekray07@gmail.com>
2024-09-04 13:31:01 -0700

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 import Link from "next/link";
 import "./globals.css";
 
